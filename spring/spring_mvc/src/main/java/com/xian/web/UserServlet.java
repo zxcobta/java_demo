cae62a07f85e18f559a6dd0f8cb16602ea60ab0a
zxcobta <1073881662@qq.com>
2021-02-05 07:20:10 +0800

    。。

--- spring/spring_mvc/src/main/java/com/xian/web/UserServlet.java
+++ spring/spring_mvc/src/main/java/com/xian/web/UserServlet.java
@@ -3,7 +3,9 @@ package com.xian.web;
 import com.xian.service.UserService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,7 +16,12 @@ public class UserServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+//        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ServletContext servletContext = this.getServletContext();
+//        ApplicationContext app = (ApplicationContext) servletContext.getAttribute("app");
+//        ApplicationContext app = WebApplicationContextUtils.getWebApplicationContext(servletContext);
+
+        ApplicationContext app = WebApplicationContextUtils.getWebApplicationContext(servletContext);
         UserService userService = app.getBean(UserService.class);
         userService.save();
     }
